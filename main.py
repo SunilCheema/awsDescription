7728baa798504b846fcfebf45420b8ece15b7a25
SunilCheema <sunil03cheema@hotmail.co.uk>
2018-08-14 09:08:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-
+#downloads the file
 def download_file(url):
     local_filename = url.split('/')[-1]
 
@@ -13,7 +13,7 @@ def download_file(url):
 
     return local_filename
 
-
+# Removes first 5 rows of the csv file
 def createNewFile():
     with open("index.csv", 'r') as f:
         with open("updated_test.csv", 'w') as f1:
@@ -23,20 +23,22 @@ def createNewFile():
             for line in f:
                 f1.write(line)
 
-r = requests.get('https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/us-east-1/index.csv')
 
-file = download_file('https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/us-east-1/index.csv')
+file = download_file('https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/eu-west-1/index.csv')
 
 createNewFile()
 
 dataframe = pd.read_csv('updated_test.csv')
 
-
+#swaps the column positions to put instance type first
 cols = dataframe.columns.tolist()
 a, b = cols.index('SKU'), cols.index('Instance Type')
 cols[b], cols[a] = cols[a], cols[b]
 
+#sorts the dataframe according to the instance type column
 dataframe = dataframe[cols]
 dataframe = dataframe.sort_values(by=['Instance Type'])
+
+#Exports the dataframe as a csv file
 dataframe.to_csv('out.csv', index = False)
 print(dataframe.head())
