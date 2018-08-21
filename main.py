fcd51ee321463bded36c8512d3b1dfa417ffdac3
EC2 Default User <ec2-user@ip-172-31-32-177.eu-west-1.compute.internal>
2018-08-21 12:57:03 +0000

    add email function

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ import os
 import csv
 import pickle
 
+import boto3
+from botocore.exceptions import ClientError
 
-def lambda_handler():
+def lambda_handler(event, context):
     download_file('https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/eu-west-1/index.csv')
     removeRows()
     dataframe = pd.read_csv('/tmp/updated_test.csv')
@@ -154,4 +156,74 @@ def findNewPrices(pastFile, currentFile):
     return differences
 
 
-lambda_handler()
+def email(content):
+    SENDER = "samueltkooy@gmail.com"
+    RECIPIENT = "samueltkooy@gmail.com"
+    
+    # Specify a configuration set. If you do not want to use a configuration
+    # set, comment the following variable, and the 
+    # ConfigurationSetName=CONFIGURATION_SET argument below.
+    #CONFIGURATION_SET = "ConfigSet"
+    
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "eu-west-1"
+    
+    # The subject line for the email.
+    SUBJECT = "Amazon EC2 status change alert"
+    
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("Check for latest changes to EC2 instances")
+                
+    # The HTML body of the email.
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+      <h1>Latest changes to EC2 instances</h1>
+      <p>{content}</p>
+    </body>
+    </html>
+                """.format(content=content)            
+    
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+    
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses',region_name=AWS_REGION)
+    
+    # Try to send the email.
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+            # If you are not using a configuration set, comment or delete the
+            # following line
+            #ConfigurationSetName=CONFIGURATION_SET,
+        )
+    # Display an error if something goes wrong.	
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
+    
